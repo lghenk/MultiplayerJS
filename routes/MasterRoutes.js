de704173f46aaa23576cf02c52e7d86be33f0ff8
Timo Heijne <me@timoheijne.nl>
2018-11-06 00:44:14 +0100

    Start of routing

--- routes/MasterRoutes.js
+++ routes/MasterRoutes.js
@@ -1,10 +1,12 @@
 const logger = require('../logger')
 const SocketServer = require('../services/SocketServer')
 
+const ClusterSpawner = require('../modules/ClusterSpawner')
+
 const socket = new SocketServer(process.env.MASTER_PORT || 2000)
 
-logger.info("Setting up master server routes");
+logger.info('Setting up master server routes')
 
-// ONLY THE MATCHMAKING AND OTHER AUTHENTICATED SERVICES SHOULD BE ALLOWED TO CONNECT HERE
+socket.on('client.message:match.initialize')
 
-socket.startServer();
+socket.startServer()
