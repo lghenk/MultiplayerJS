067a1ee058a3057663586f5996bc1aadf926e23b
Timo Heijne <me@timoheijne.nl>
2018-11-06 00:08:40 +0100

    Created a few files for architecture visualization

---
+++ config/WhitelistedServers.js
@@ -0,0 +1,17 @@
+class WhitelistItem {
+    constructor(ip, key) {
+        this.ip = ip;
+        this.key = key;
+    }
+}
+
+const Whitelist = {
+    "Matchmaking": [
+
+    ],
+    "Multiplayer": [
+
+    ]
+}
+
+module.exports = Whitelist;
