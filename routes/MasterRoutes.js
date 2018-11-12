04aaf9d8549d81b4faef5e1367fd96cf06aeaf0a
Timo Heijne <me@timoheijne.nl>
2018-11-12 18:31:52 +0100

    Added listener to match.initialize

--- routes/MasterRoutes.js
+++ routes/MasterRoutes.js
@@ -7,6 +7,6 @@ const socket = new SocketServer(process.env.MASTER_PORT || 2000)
 
 logger.info('Setting up master server routes')
 
-socket.on('client.message:match.initialize')
+socket.on('client.message:match.initialize', MatchController.InitializeMatch)
 
 socket.startServer()
