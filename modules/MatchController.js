b947ef4c9489f8d35f7634ca95136e6cde02d664
Timo Heijne <me@timoheijne.nl>
2018-11-12 18:31:40 +0100

    Handle incoming and outgoing request for a match

--- modules/MatchController.js
+++ modules/MatchController.js
@@ -1,9 +1,19 @@
 const ClusterSpawner = require('./ClusterSpawner')
+const logger         = require('../logger')
+const uuidV4         = require('uuid/v4')
 
+// [0] = Matchmaking Reference ID
 module.exports.InitializeMatch = (data) => {
   // Generate a match ID & Auth Token
   // Send a signal to cluster spawner for a game cluster
   // Send ID, Auth token, IP & Port back to matchmaking
+  const ref = data.args[0];
+  const matchId = uuidV4();
+  const authToken = 'auth-'+uuidV4();
+  logger.info(`Initializing match with id: ${matchId}`)
 
+  const port = ClusterSpawner.InitializeCluster(matchId, authToken);
   const IP = process.env.IP_ADDRESS || "127.0.0.1"
+
+  data.client.send('match.initialize.success', ref, matchId, authToken, IP, port)
 }
