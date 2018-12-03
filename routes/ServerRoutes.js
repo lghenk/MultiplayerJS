57db2a06f5147c732a2b5206bf21b3d5a5589bcf
Timo Heijne <me@timoheijne.nl>
2018-12-03 11:04:37 +0100

    Styling fix

--- routes/ServerRoutes.js
+++ routes/ServerRoutes.js
@@ -6,13 +6,13 @@ const socket = new SocketServer(0)
 
 socket.on('socket.listening', () => {
   // Server is now listening and ready for connections
-  process.send({ event: 'listening', port: socket._server.address().port })
+  process.send({event: 'listening', port: socket._server.address().port});
 })
 
 process.on('message', (data) => {
-  if (data.event == 'init') {
+  if(data.event == 'init') {
     logger.info(`Game Cluster ${cluster.worker.id}: Initializing with port ${data.port}`)
-    socket._port = data.port
+    socket._port = data.port;
     socket.startServer()
   }
 })
