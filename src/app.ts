f6308e4b05918444e3ab2f8955a4bad236bc65c8
Timo Heijne <me@timoheijne.nl>
2018-12-03 11:30:03 +0100

    Base convert to typescript & eslint

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const cluster = require('cluster');
+const os = require('os');
+const logger = require('./logger');
+
+const serverType = process.env.SERVER_TYPE || 'MASTER';
+const numCPUs = os.cpus().length;
+
+if (cluster.isMaster) {
+  logger.info(`Initializing multiplayer ${serverType} server spawner. Detected ${numCPUs} CPU's.`);
+
+  if (serverType === 'MASTER') {
+    require('./routes/MasterRoutes');
+  } else if (serverType === 'SLAVE') {
+    require('./routes/SlaveRoutes');
+  } else {
+    logger.fatal('Server Type not found!');
+  }
+} else {
+  require('./routes/ServerRoutes');
+}
