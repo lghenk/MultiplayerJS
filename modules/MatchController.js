30fd22aeb7ba9998c5de74061203761d30fdf347
Timo Heijne <me@timoheijne.nl>
2018-12-03 11:04:26 +0100

    Updated comment for expected input, now expecting worker and port from initcluster

--- modules/MatchController.js
+++ modules/MatchController.js
@@ -4,18 +4,25 @@ const uuidV4 = require('uuid/v4')
 
 const matches = []
 
-// [0] = Matchmaking Reference ID
+// [0] = Matchmaking Reference ID, [1] = Expected num players, [2] = max players
 module.exports.InitializeMatch = (data) => {
   // Generate a match ID & Auth Token
   // Send a signal to cluster spawner for a game cluster
   // Send ID, Auth token, IP & Port back to matchmaking
   const ref = data.args[0]
+
+  if(ref === undefined) {
+    return data.client.send('match.initialize.failed', "no reference provided")
+  }
+
   const matchId = uuidV4()
   const authToken = 'auth-' + uuidV4()
   logger.info(`Initializing match with id: ${matchId}`)
 
-  const port = ClusterSpawner.InitializeCluster(matchId, authToken)
+  const {port, worker} = ClusterSpawner.InitializeCluster(matchId, authToken)
   const IP = process.env.IP_ADDRESS || '127.0.0.1'
+
+  worker.send({event: 'setup', max_players: data.args[1], expected_players: data.args[2]})
 
   data.client.send('match.initialize.success', ref, matchId, authToken, IP, port)
 }
